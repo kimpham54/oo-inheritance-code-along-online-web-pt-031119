df36212728b6f8aeb82abd868e79f2a74fce018b
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-28 21:11:03 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,19 +1,7 @@
 require_relative "./vehicle.rb"
-class Vehicle
+class Car < Vehicle
 
-  attr_accessor :wheel_size, :wheel_number
+end
 
-  def initialize(wheel_size, wheel_number)
-    @wheel_size = wheel_size
-    @wheel_number = wheel_number
-  end
-
-  def go
-    "vrrrrrrrooom!"
-  end
-
-  def fill_up_tank
-    "filling up!"
-  end
 
 end
